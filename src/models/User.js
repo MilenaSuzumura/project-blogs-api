af70b1d298628aac534343ea27bbfa182a3b7d38
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-11-11 22:12:29 -0300

    requisito 2 realizado

---
+++ src/models/User.js
@@ -0,0 +1,15 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  });
+  return User;
+};
