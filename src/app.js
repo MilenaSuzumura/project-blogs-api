82010599fcf01e33392a26857e67b0eb85477f5c
Milena Suzumura <milena.suzumura11@gmail.com>
2023-04-05 18:55:21 -0300

    fix: organizei o app

--- src/app.js
+++ src/app.js
@@ -1,22 +1,16 @@
 const express = require('express');
-
-// ...
-
-const app = express();
-
-app.use(express.json());
-
-// ...
-
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 const loginRouter = require('./routes/login.route');
 const userRouter = require('./routes/user.route');
 const categoriesRouter = require('./routes/categories.route');
 const postRouter = require('./routes/post.route');
 
+const app = express();
+
+app.use(express.json());
+
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
 app.use('/categories', categoriesRouter);
 app.use('/post', postRouter);
+
 module.exports = app;
