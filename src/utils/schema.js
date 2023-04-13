7c9b898f2935b671ff717ff5ec9ca45d116e6c53
Milena Suzumura <milena.suzumura11@gmail.com>
2023-04-13 18:53:25 -0300

    feat: create validation createUserValidation

--- src/utils/schema.js
+++ src/utils/schema.js
@@ -1,10 +1,27 @@
 const Joi = require('joi');
 
-const emailAndPassword = Joi.object({
+const emailAndPasswordValidation = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
 
+const createUserValidation = Joi.object({
+  displayName: Joi.string().min(8).required()
+    .messages({
+      'string.min': '"displayName" length must be at least 8 characters long',
+    }),
+  email: Joi.string().email().required()
+    .messages({
+      'string.email': '"email" must be a valid email',
+    }),
+  password: Joi.string().min(6).required()
+    .messages({
+      'string.min': '"password" length must be at least 6 characters long',
+    }),
+  image: Joi.string(),
+});
+
 module.exports = {
-  emailAndPassword,
+  emailAndPasswordValidation,
+  createUserValidation,
 };
