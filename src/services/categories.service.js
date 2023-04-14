c2a7abb745ff86aa8bceb7cbe0d056c17f6f145d
Milena Suzumura <milena.suzumura11@gmail.com>
2023-04-14 11:29:51 -0300

    fix: ajeitei o getCategories

--- src/services/categories.service.js
+++ src/services/categories.service.js
@@ -1,18 +1,14 @@
-const { Category } = require('../models');
 const { verifyParametersCategories } = require('../utils/verify/verify.categories');
-const { registerCategory } = require('../callModel/category.callModel');
+const { registerCategory, getAllCategory } = require('../callModel/category.callModel');
 
 const verifyParameters = (info) => verifyParametersCategories(info);
 
 const register = async (info) => registerCategory(info);
 
-const everyCategories = async () => {
-  const categories = await Category.findAll();
-  return categories;
-};
+const getCategories = async () => getAllCategory();
 
 module.exports = {
   verifyParameters,
   register,
-  everyCategories,
+  getCategories,
 };
