b07d1ac7e8bbf48ccfd3426dcf8e4876e87663b6
Milena Suzumura <milena.suzumura11@gmail.com>
2023-04-15 20:13:43 -0300

    fix: modify rota

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const postController = require('../controllers/post.controller');
 const verifyToken = require('../middleware/verifyToken');
+const verifySameUser = require('../middleware/verifySameUser');
 
 const postRouter = Router();
 
 postRouter.post('/', verifyToken, postController.registerPost);
 postRouter.get('/', verifyToken, postController.getPosts);
-postRouter.get('/:id', verifyToken, postController.exibeIdPost);
-postRouter.put('/:id', verifyToken, postController.alterar);
+postRouter.get('/:id', verifyToken, postController.findByIdPost);
+postRouter.put('/:id', verifyToken, verifySameUser, postController.modifyPost);
 postRouter.delete('/:id', verifyToken, postController.deletePost);
 
 module.exports = postRouter;
