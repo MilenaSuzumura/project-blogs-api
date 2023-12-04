ec63978f059145c42ba07a07eed0d0f4c68387b1
Milena Suzumura <milena.suzumura11@gmail.com>
2023-12-04 17:41:59 -0300

    feat: Fiz alguns testes da rota user

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -12,7 +12,11 @@ const { Model } = require('sequelize');
 chai.use(chaiHttp);
 
 const { allUsers } = require('./mocks/user');
-const Authorization = require('./mocks/authorization');
+const {
+  Authorization,
+  invalidToken,
+  messageNotFound,
+  messageInvalidToken } = require('./mocks/authorization');
 
 describe('Teste a rota /user', () => {
   beforeEach(() => sinon.restore());
@@ -20,12 +24,72 @@ describe('Teste a rota /user', () => {
   it('Testa se a rota get /user retorna todos os usuarios', async function () {
     sinon.stub(Model, 'findAll').resolves(allUsers);
 
-    const response = await (chai.request(app).get('/user').set({
+    const response = await (chai.request(app).get('/user').set({ 
       Authorization,
     }));
 
     expect(response.status).to.be.equal(200);
     expect(response.body).to.deep.equal(allUsers);
+  });
+
+  it('Testa se a rota get /user não retorna todos os usuarios se não tiver o token', async function () {
+    const response = await (chai.request(app).get('/user'));
+
+    expect(response.status).to.be.equal(401);
+    expect(response.body).to.deep.equal(messageNotFound);
+  });
+
+  it('Testa se a rota get /user não retorna todos os usuarios se o token for invalido', async function () {
+    const response = await (chai.request(app).get('/user').set({ 
+      'Authorization': invalidToken
+    }));
+
+    expect(response.status).to.be.equal(401);
+    expect(response.body).to.deep.equal(messageInvalidToken);
+  });
+
+  it('Testa se a rota get /user/:id retorna o usuario do id', async function () {
+    sinon.stub(Model, 'findOne').resolves(allUsers[1]);
+
+    const response = await (chai.request(app).get('/user/1').set({
+      Authorization,
+    }));
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.not.deep.equal(allUsers[0]);
+    expect(response.body).to.deep.equal(allUsers[1]);
+  });
+
+  it('Testa se a rota get /user/:id não retorna o usuario do id se não tiver o token', async function () {
+    const response = await (chai.request(app).get('/user/1'));
+
+    expect(response.status).to.be.equal(401);
+    expect(response.body).to.deep.equal(messageNotFound);
+  });
+
+  it('Testa se a rota get /user/id não retorna o usuario do id se o token for invalido', async function () {
+    const response = await (chai.request(app).get('/user/1').set({ 
+      'Authorization': invalidToken
+    }));
+
+    expect(response.status).to.be.equal(401);
+    expect(response.body).to.deep.equal(messageInvalidToken);
+  });
+
+  it('Testa se a rota delete /user/me não exclui o usuario se não tiver o token', async function () {
+    const response = await (chai.request(app).delete('/user/me'));
+
+    expect(response.status).to.be.equal(401);
+    expect(response.body).to.deep.equal(messageNotFound);
+  });
+
+  it('Testa se a rota delete /user/me não exclui o usuario se o token for invalido', async function () {
+    const response = await (chai.request(app).delete('/user/me').set({ 
+      'Authorization': invalidToken
+    }));
+
+    expect(response.status).to.be.equal(401);
+    expect(response.body).to.deep.equal(messageInvalidToken);
   });
 })
 
